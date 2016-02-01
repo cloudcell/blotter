10f78c86ffe381c2eabbde14ceb24a3c622d9497
cloudcello <ef256aff@opayq.com>
2016-02-02 02:33:15 +0300

    Test Setup: Attempt +1

---
+++ tests/runitAddTxn.R
@@ -0,0 +1,55 @@
+# Author: Peter Carl, RUnit port by Ben McCann
+
+
+
+test.addTxn <- function() {
+
+  on.exit({
+    # remove objects created by unit tests
+    try(rm_currencies("USD"))
+    try(rm_stocks(symbols))
+    # try(rm(list=p, pos=.blotter))
+    try(rm(list="pName"))
+    ls(pos=.blotter)
+    # try(rm(list="portfolio.runitAddTxn",pos=.blotter)) # TODO: write rm_portfolio(x)
+    ls_portfolios()
+    ls_accounts()
+    try(rm_portfolios("runitAddTxn"))
+    try(rm("IBM", pos = .GlobalEnv))
+    # getPortfolio("runitAddTxn")
+  })
+
+  currency("USD")
+  symbols <- c("IBM")
+  for (symbol in symbols){
+    stock(symbol, currency="USD", multiplier=1)
+  }
+  data("IBM", package="blotter")
+
+  # Initialize a portfolio object 'p'
+  # Creating portfolio:
+  pName <- initPortf("runitAddTxn", symbols=symbols)
+
+  # Trades must be made in date order.
+  # Make a couple of trades in IBM
+  addTxn(pName, "IBM", '2007-01-03',  50,  96.5,  TxnFees=-0.05 * 50)
+  addTxn(pName, "IBM", '2007-01-04', -50,  97.1,  TxnFees=-0.05 * 50)
+  addTxn(pName, "IBM", '2007-01-08', -10,  99.2,  TxnFees=-0.05 * 10)
+  addTxn(pName, "IBM", '2007-01-09', -10, 100.1,  TxnFees=-0.05 * 10)
+  addTxn(pName, "IBM", '2007-01-17', -10, 100.25, TxnFees=-0.05 * 10)
+  addTxn(pName, "IBM", '2007-01-19',  30,  95,    TxnFees=-0.05 * 30)
+  addTxn(pName, "IBM", '2007-01-22',  25,  96.3,  TxnFees=-0.05 * 25)
+  addTxn(pName, "IBM", '2007-01-23',  25,  96.42, TxnFees=-0.05 * 25)
+  addTxn(pName, "IBM", '2007-01-26', -25,  97.52, TxnFees=-0.05 * 25)
+  addTxn(pName, "IBM", '2007-01-31', -25,  98.80, TxnFees=-0.05 * 25)
+
+  portfolio <- getPortfolio(pName)
+  transactions <- portfolio$symbols[["IBM"]]$txn
+  checkEquals(-13, sum(transactions$Txn.Fees))
+  checkEquals(0, sum(transactions$Txn.Qty))
+
+  # TODO: fix bug in calcPortfSummary
+  # summary <- calcPortfSummary(portfolio)
+}
+
+#----------------------------------------------------------------------------- -
---
+++ tests/runitUpdatePortf.R
@@ -0,0 +1,47 @@
+# Author: Peter Carl, RUnit port by Ben McCann
+
+test.txnFees <- function() {
+
+  on.exit({
+    # remove objects created by unit tests
+    try(rm_currencies("USD"))
+    try(rm_stocks(symbols))
+    # try(rm(list=c(p1,a1), pos=.blotter))
+    ls_portfolios()
+    ls_accounts()
+    ls(pos=.blotter)
+
+    try(rm_portfolios(x=c("p1runitUpdatePortf")))
+    try(rm_portfolios(x=c("p2runitUpdatePortf")))
+    try(rm_accounts(x=c("a1runitUpdatePortf")))
+    try(rm_accounts(x=c("a2runitUpdatePortf")))
+
+    try(rm("IBM", pos = .GlobalEnv))
+  })
+
+  currency("USD")
+  symbols <- c("IBM")
+  for (symbol in symbols){
+    stock(symbol, currency="USD", multiplier=1)
+  }
+  data("IBM", package="blotter")
+
+  ## simple portfolio with one transaction
+  p1 <- initPortf(name="p1runitUpdatePortf", symbols=symbols)
+  p1 <- addTxn(Portfolio="p1runitUpdatePortf", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=-0.05*100)
+  p1 <- updatePortf(Portfolio="p1runitUpdatePortf", Dates='2007-01-03::2007-01-10')
+  a1 <- initAcct(name="a1runitUpdatePortf", portfolios="p1runitUpdatePortf")
+  a1 <- updateAcct(a1,'2007-01')
+  a1 <- updateEndEq(a1,'2007-01')
+
+  ## (really) simple transaction cost function
+  fiveCents <- function(qty, prc, ...) return(-0.05*qty)
+  p2 <- initPortf(name="p2runitUpdatePortf", symbols=symbols)
+  p2 <- addTxn(Portfolio="p2runitUpdatePortf", Symbol="IBM", TxnDate='2007-01-04', TxnQty=100, TxnPrice=96.5, TxnFees=fiveCents)
+  p2 <- updatePortf(Portfolio="p2runitUpdatePortf", Dates='2007-01-03::2007-01-10')
+  a2 <- initAcct(name="a2runitUpdatePortf", portfolios="p2runitUpdatePortf")
+  a2 <- updateAcct(a2,'2007-01')
+  a2 <- updateEndEq(a2,'2007-01')
+
+  checkEquals(getAccount(a1)$summary$End.Eq, getAccount(a2)$summary$End.Eq)
+}
